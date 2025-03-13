0638ae89fdcccd3f714b2ca6312dc8b36b5a142f
Brad Montgomery <brad@bradmontgomery.net>
2025-03-12 19:55:59 -0500

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,27 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  // Temporary code
-  alert("Hello world!");
+  // Old-style Cookies
+  const now = new Date();
+  document.cookie = `now=${now}; SameSite=None; Secure`;
+  document.cookie = "course=WebProgramming2025; SameSite=None; Secure";
+  document.getElementById("old-cookies").innerText = document.cookie;
+
+  // Look-style cookies / CookieStore
+  const cookieStore = window.cookieStore;
+  cookieStore.set({ name: "username", value: "bradmontgomery" }).then(
+    () => {
+      console.log("Cookie set using cookieStore");
+    },
+    (reason) => {
+      console.error("Unable to set cookie: " + reason);
+    }
+  );
+  cookieStore.get("username").then(
+    (obj) => {
+      const elt = document.getElementById("new-cookies");
+      elt.innerText = `${obj.name}=${obj.value}`;
+    },
+    (reason) => {
+      console.error("Unable to read cookie: " + reason);
+    }
+  );
 });
